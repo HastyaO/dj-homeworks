7dc244b1b382af3ebebc541b0a6b738b495d5404
Nastya Ovchinnikova <hastyao52@gmail.com>
2024-12-20 20:48:17 +0300

    3.1 task

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 
+from rest_framework import routers
+
+from .views import SensorView, SingleSensorView, MeasurementView
+
+router = routers.DefaultRouter()
+
+app_name = "measurement"
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorView.as_view()),
+    path('sensors/<int:pk>/', SingleSensorView.as_view()),
+    path('measurements/', MeasurementView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,29 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import viewsets, mixins
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+class SensorViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class DetailSensorViewSet(
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Sensor.objects.prefetch_related('measurements')
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementViewSet(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
