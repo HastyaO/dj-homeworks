21b67dc26881e8ecd0f90ad1ee025e58134e26f7
Nastya Ovchinnikova <hastyao52@gmail.com>
2024-12-20 20:25:36 +0300

    3th task

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_obj = Phone.objects.all()
+    sort_value = request.GET.get('sort')
+    if sort_value == 'min_price':
+        phone_obj = phone_obj.order_by('price')
+    elif sort_value == 'max_price':
+        phone_obj = phone_obj.order_by('-price')
+    elif sort_value == 'name':
+        phone_obj = phone_obj.order_by('name')
+    context = {
+        'phones': phone_obj,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone =  get_object_or_404(Phone, slug=slug)
+    context = {
+        'phone': phone,
+    }
     return render(request, template, context)
